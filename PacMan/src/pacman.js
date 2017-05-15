97d2ff19a428743aada78ea04c977d6f10099c1c
Miguel Perello garcia <canyavall@gmail.com>
2017-05-15 09:37:21 +0200

    Mistake with pacman file

--- PacMan/src/pacman.js
+++ PacMan/src/pacman.js
@@ -15,38 +15,7 @@ export class Pacman extends Mob {
    * @return {[type]} [description]
    */
   render(){
-<<<<<<< HEAD
-    //Change mouth position
-    let arc1Start = 0.25 * Math.PI,
-        arc1End = 1.25 * Math.PI,
-        arc2Start = 0.75 * Math.PI,
-        arc2End = 1.75 * Math.PI,
-        eyeX = this.position[0] + 2,
-        eyeY = this.position[1] - 6;
-
-    if (this.direction[0] === true){
-      arc2Start = 1.75 * Math.PI;
-      arc2End = 0.75 * Math.PI;
-      eyeX = this.position[0] - 6,
-      eyeY = this.position[1] - 2;
-    }
-    if (this.direction[1] === true){
-      arc1Start = 0.75 * Math.PI;
-      arc1End = 1.75 * Math.PI;
-      arc2Start = 1.25 * Math.PI;
-      arc2End = 0.25 * Math.PI;
-      eyeX = this.position[0] + 6,
-      eyeY = this.position[1] + 2;
-    }
-    if (this.direction[2] === true){
-      arc1Start = 1.25 * Math.PI;
-      arc1End = 0.25 * Math.PI;
-      arc2Start = 1.75 * Math.PI;
-      arc2End = 0.75 * Math.PI;
-    }
-=======
     let headDirection = this.checkHeadDirection();
->>>>>>> master
     this.ctx.beginPath();
     this.ctx.arc(this.position[0], this.position[1], this.radius, headDirection["arc1Start"], headDirection["arc1End"], false);
     this.ctx.fillStyle = "rgb(255, 255, 0)";
@@ -55,37 +24,11 @@ export class Pacman extends Mob {
     this.ctx.arc(this.position[0], this.position[1], this.radius, headDirection["arc2Start"], headDirection["arc2End"], false);
     this.ctx.fill();
     this.ctx.beginPath();
-<<<<<<< HEAD
-    this.ctx.arc(eyeX, eyeY, 2, 0 * Math.PI, 1.75 * Math.PI, false);
+    this.ctx.arc(headDirection["eyeX"], headDirection["eyeY"], 2, 0 * Math.PI, 1.75 * Math.PI, false);
     this.ctx.fillStyle = "black";
     this.ctx.fill();
-=======
-    this.ctx.arc(headDirection["eyeX"], headDirection["eyeY"], 2, 0 * Math.PI, 1.75 * Math.PI, false);
-    this.ctx.fillStyle = "black";
-    this.ctx.fill();
->>>>>>> master
   };
 
-<<<<<<< HEAD
-    //Modify direction
-    if (this.direction[0] && this.go[0]) this.position[1] -= this.speed;
-    if (this.direction[1] && this.go[1]) this.position[1] += this.speed;
-    if (this.direction[2] && this.go[2]) this.position[0] -= this.speed;
-    if (this.direction[3] && this.go[3]) this.position[0] += this.speed;
-  }
-  /**
-   * [boardCollision description]
-   * @param  {[integer]} xPositionPlusRadius  Radius plus x position
-   * @param  {[integer]} xPositionMinusRadius Radius minus x position
-   * @param  {[integer]} yPositionPlusRadius  Radius plus y position
-   * @param  {[integer]} yPositionMinusRadius Radius minus y position
-    */
-    boardCollision(xPositionPlusRadius, xPositionMinusRadius, yPositionPlusRadius, yPositionMinusRadius){
-      if (yPositionMinusRadius === 0) this.go[0] = false;
-      if (yPositionPlusRadius === this.ctx.canvas.clientHeight) this.go[1] = false;
-      if (xPositionMinusRadius === 0) this.go[2] = false;
-      if (xPositionPlusRadius === this.ctx.canvas.clientWidth) this.go[3] = false;
-=======
   checkHeadDirection(){
     //Change mouth position
     let head = {};
@@ -94,13 +37,12 @@ export class Pacman extends Mob {
     head["arc2Start"] = 0.75 * Math.PI;
     head["arc2End"] = 1.75 * Math.PI;
     head["eyeX"] = this.position[0] + 2,
-    head["eyeY"] = this.position[1] - 6;
+    head["eyeY"] = this.position[1] - 6;
     if (this.direction[0] === true){
       head["arc2Start"] = 1.75 * Math.PI;
       head["arc2End"] = 0.75 * Math.PI;
       head["eyeX"] = this.position[0] - 6,
-      head["eyeY"] = this.position[1] - 2;
->>>>>>> master
+      head["eyeY"] = this.position[1] - 2;
     }
     if (this.direction[1] === true){
       head["arc1Start"] = 0.75 * Math.PI;
@@ -108,7 +50,7 @@ export class Pacman extends Mob {
       head["arc2Start"] = 1.25 * Math.PI;
       head["arc2End"] = 0.25 * Math.PI;
       head["eyeX"] = this.position[0] + 6,
-      head["eyeY"] = this.position[1] + 2;
+      head["eyeY"] = this.position[1] + 2;
     }
     if (this.direction[2] === true){
       head["arc1Start"] = 1.25 * Math.PI;
