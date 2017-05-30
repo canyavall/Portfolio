3f884b38454753605b727ca2ca31df337cb850e8
Miguel Perello garcia <canyavall@gmail.com>
2017-05-30 23:38:57 +0200

    maxSUmArrays

--- ThinkCode/maxSumArray.js
+++ ThinkCode/maxSumArray.js
@@ -16,16 +16,36 @@
 
 function maxSequence(arrayNum){
   let valid = false;
+  let finalArray = [];
+  let bufferArray = [];
+  let sumNumber = 0;
   for (var i = 0; i < arrayNum.length; i++){
     if (arrayNum[i] >= 0) valid = true;
+
+    if (arrayNum[i] < 0 && finalArray.length == 0) continue; //ignore first number if it is negative
+    if (arrayNum[i] > sumNumber) {// If the number given is bigger than final
+      finalArray = [arrayNum[i]];
+      continue;
+    }
+
+    if (arrayNum[i] > 0 & bufferArray.length == 0) finalArray.push(arrayNum[i]); //If it is possitve and we are not calculating the buffer
+
+
+    if(arrayNum[i] < 0 ){
+      if (bufferArray.length == 0)  bufferArray = finalArray;
+
+    }
+
+
+
+
+    sumNumber = finalArray.reduce((x, y) => x + y)
   }
+console.log(finalArray);
   if (!valid) return null;
-
-
+  return sumNumber;
 
 }
 
-
-
-maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-maxSequence([-2, -5])
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4, -1]));// should be 6: [4, -1, 2, 1]
+//console.log(maxSequence([-2,1,-5])) // should be 1
